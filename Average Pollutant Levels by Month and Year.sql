da66312e3d2bb552a37e107525a45153c8c7416d
Abhranil Das <9abhranil@gmail.com>
2025-08-10 22:08:09 +0530

    Update Average Pollutant Levels by Month and Year.sql

--- Average Pollutant Levels by Month and Year.sql
+++ Average Pollutant Levels by Month and Year.sql
@@ -8,7 +8,6 @@ SELECT
     ROUND(AVG(pg.SO2),0) AS avg_SO2,
     ROUND(AVG(pg.CO),2) AS avg_CO,
     ROUND(AVG(pg.Ozone),0) AS avg_Ozone,
-    ROUND(AVG(pg.AQI),0) AS avg_AQI
 FROM date d
 LEFT JOIN Pollutants_PM pm ON d.Date_id = pm.Date_id
 LEFT JOIN Pollutants_Gas pg ON d.Date_id = pg.Date_id
